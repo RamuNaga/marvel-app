23f558917f9d8c994996df8bbbe073ff2c89af60
Ramu <ramu.vuppanapalli@gmail.com>
2024-02-13 19:51:01 +0000

    Unit testing changes

--- src/core/components/breadcumb/breadcumb.component.ts
+++ src/core/components/breadcumb/breadcumb.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   ActivatedRoute,
@@ -8,7 +8,11 @@ import {
   RouterModule,
   RouterOutlet,
 } from '@angular/router';
-import { distinctUntilChanged, filter } from 'rxjs';
+import { Observable, distinctUntilChanged, filter } from 'rxjs';
+import {
+  Breadcrumb,
+  BreadcrumbService,
+} from '../../services/breadcrumb.service';
 
 export type IBreadCrumb = {
   label: string;
@@ -18,65 +22,13 @@ export type IBreadCrumb = {
 @Component({
   selector: 'app-breadcumb',
   standalone: true,
-  imports: [RouterModule, NgIf, NgFor],
+  imports: [RouterModule, NgIf, NgFor, AsyncPipe],
   templateUrl: './breadcumb.component.html',
   styleUrl: './breadcumb.component.scss',
 })
-export class BreadcumbComponent implements OnInit {
-  public breadcrumbs: IBreadCrumb[];
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-  }
-
-  ngOnInit() {
-    this.router.events
-      .pipe(
-        filter((event: Event) => event instanceof NavigationEnd),
-        distinctUntilChanged()
-      )
-      .subscribe(() => {
-        this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-      });
-  }
-
-  /**
-   * Recursively build breadcrumb according to activated route.
-   * @param route
-   * @param url
-   * @param breadcrumbs
-   */
-  buildBreadCrumb(
-    route: ActivatedRoute,
-    url: string = '',
-    breadcrumbs: IBreadCrumb[] = []
-  ): IBreadCrumb[] {
-    let label =
-      route.routeConfig && route.routeConfig.data
-        ? route.routeConfig.data['breadcrumb']
-        : '';
-    let path =
-      route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
-
-    const lastRoutePart = path?.split('/').pop();
-    const isDynamicRoute = lastRoutePart?.startsWith(':');
-    if (isDynamicRoute && !!route.snapshot && lastRoutePart) {
-      const paramName = lastRoutePart.split(':')[1];
-      path = path?.replace(lastRoutePart, route.snapshot.params[paramName]);
-      label = route.snapshot.params[paramName];
-    }
-    const nextUrl = path ? `${url}/${path}` : url;
-
-    const breadcrumb: IBreadCrumb = {
-      label: label,
-      url: nextUrl,
-    };
-    const newBreadcrumbs = breadcrumb.label
-      ? [...breadcrumbs, breadcrumb]
-      : [...breadcrumbs];
-    if (route.firstChild) {
-      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
-    }
-    return newBreadcrumbs;
+export class BreadcumbComponent {
+  breadcrumbs$: Observable<Breadcrumb[]>;
+  constructor(private readonly breadcrumbService: BreadcrumbService) {
+    this.breadcrumbs$ = breadcrumbService.breadcrumbs$;
   }
 }
